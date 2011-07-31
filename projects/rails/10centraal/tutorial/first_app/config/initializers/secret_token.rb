a502f4a91e2a2a4da39d10ed4ef38b47d6572733
Nilo-El-Inca <nilo.paredes@gmail.com>
2011-07-31 16:10:45 +0200

    removing the directory

--- projects/rails/10centraal/tutorial/first_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = 'b07e591e37146678e8ef267e60a50953f0f8919ae0090f458ae77e1e327808a7429b90e73cf31993e0df223dfe68cb6836f0b1ce8a11c522f2f7beb4a88b8bde'
